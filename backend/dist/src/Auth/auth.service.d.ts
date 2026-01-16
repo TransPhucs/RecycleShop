e1a7630f31dd2f054c6f2f5ee03bfa9dccf57ca2
Tran Trong Phuc <tphuc290923@gmail.com>
2026-01-16 21:02:51 +0700

    commit done login basic

---
+++ backend/dist/src/Auth/auth.service.d.ts
@@ -0,0 +1,15 @@
+import { PrismaService } from "../../prisma/prisma.service";
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from "../User/user.service";
+export declare class AuthService {
+    private readonly prismaService;
+    private readonly userService;
+    private readonly jwtService;
+    constructor(prismaService: PrismaService, userService: UserService, jwtService: JwtService);
+    validate(email: string, password: string): Promise<any>;
+    authLogin(user: any): Promise<{
+        token: string;
+        user: any;
+    }>;
+    authRegister(user: any): Promise<import("../User/DTO/user.response.dto").UserResponseDto>;
+}
